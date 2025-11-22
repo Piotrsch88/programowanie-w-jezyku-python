29b5d11a1c25f1b802ea9028bef6c9191fb5cf19
piotr schuetz <piotr.schuetz@edu.uekat.pl>
2025-11-22 16:10:24 +0100

    add zad1 file

---
+++ zad1.py
@@ -0,0 +1,10 @@
+print("tomasz", "arkadiusz", "mateusz", "damian", "stanisław")
+numbers = [1,2,3,4,5]
+number_multiply2 = []
+for number in numbers:
+
+    number_multiply2.append (number * 2)
+    print (number * 2)
+
+
+
