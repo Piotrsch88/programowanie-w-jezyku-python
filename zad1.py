1c678654da6b5a5454f3021fdc0748314def629e
piotr schuetz <piotr.schuetz@edu.uekat.pl>
2025-12-03 21:27:34 +0100

    dodanie petli w celu wyswietlenia listy liczb pomnozonych przez 2

--- zad1.py
+++ zad1.py
@@ -1,10 +1,20 @@
 print("tomasz", "arkadiusz", "mateusz", "damian", "stanisław")
-numbers = [1,2,3,4,5]
-number_multiply2 = []
-for number in numbers:
+names = ["tomasz", "arkadiusz", "mateusz", "damian", "stanisław"]
 
-    number_multiply2.append (number * 2)
-    print (number * 2)
+numbers = [1,2,3,4,5]
+numbers_multiply_by_two = []
+for number in numbers:
+    numbers_multiply_by_two.append (number * 2)
+    # print (number * 2)
+# for name in names:
+#     print(name)
+
+for number_multiply_by_two in numbers_multiply_by_two:
+    print(number_multiply_by_two)
+
+
+
+
 
 
 
