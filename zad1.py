5cba7c4f62d5aaf4d982f650fc8c01b14e4dd11a
piotr schuetz <piotr.schuetz@edu.uekat.pl>
2025-12-18 21:40:07 +0100

    aktualizacja danych

--- zad1.py
+++ zad1.py
@@ -1,16 +1,45 @@
-print("tomasz", "arkadiusz", "mateusz", "damian", "stanisław")
-names = ["tomasz", "arkadiusz", "mateusz", "damian", "stanisław"]
+#lista imion
+def print_names(names):
+    for name in names:
+        print(name)
 
-numbers = [1,2,3,4,5]
-numbers_multiply_by_two = []
-for number in numbers:
-    numbers_multiply_by_two.append (number * 2)
-    # print (number * 2)
-# for name in names:
-#     print(name)
+print_names( ["tomasz", "arkadiusz", "mateusz", "damian", "stanisław"])
 
-for number_multiply_by_two in numbers_multiply_by_two:
-    print(number_multiply_by_two)
+#lista liczb przemnożonych przez 2 pętla for
+def multiply_numbers_by_two(numbers):
+    numbers_multiplied_by_two = []
+
+    for number in numbers:
+        number_multiplied_by_two = number * 2
+        numbers_multiplied_by_two.append(number_multiplied_by_two)
+
+    return numbers_multiplied_by_two
+
+print(multiply_numbers_by_two([1,2,3,4,5]))
+
+#lista liczb przemnożonych przez 2 lista składana
+def multiply_numbers_by_two_using_list_comprehension(numbers):
+    return [number * 2 for number in numbers]
+
+print (multiply_numbers_by_two_using_list_comprehension([1,2,3,4,5]))
+
+
+# # lista 10 cyfr wyświetla liczby parzyste
+# numbers1 = [1,2,3,4,5,6,7,8,9,10]
+# for numbers1 in range (1,11):
+#     print(numbers1)
+# for numbers1 in range (1,11):
+#     if numbers1 % 2 == 0:
+#         print(numbers1)
+#
+# # #lista 10 cyfr wyświetla co drugi element
+# numbers2 = [11, 12, 13, 14, 15,16, 17, 18, 19,20]
+# for numbers2 in range (11,21):
+#     print (numbers2)
+# for numbers2 in range (11,21):
+#     if numbers2 % 2 == 0:
+#         print(numbers2)
+
 
 
 
