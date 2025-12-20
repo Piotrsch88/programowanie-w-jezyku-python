fb0e98029cbbe4b171582769e04f69470f587b3c
piotr schuetz <piotr.schuetz@edu.uekat.pl>
2025-12-20 12:40:54 +0100

    stworzenie funkcji łączącej dwie listy w jedna usuniecie duplikatow i podniesienie wszytskich elementow do potegi trzeciej

---
+++ Zad6operatory.py
@@ -0,0 +1,6 @@
+# połączenie 2 list w jedną, usunięcie dupilkatów i podniesienie wszystkich elementów do 3 potęgi
+list1 = [1, 2, 3]
+list2 = [3, 5, 6]
+def check_number(list1: list, list2: list) -> list:
+    return list(dict.fromkeys(x**3 for x in list1 + list2))
+print(check_number(list1, list2))
