0eedff00dc8408e092a9d42abb5f3b6de655ab87
piotr schuetz <piotr.schuetz@edu.uekat.pl>
2025-12-21 16:18:08 +0100

    stworzenie klasy posiadającej dwa parametry oraz jedną metodę

---
+++ zad1oop.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks  # lista ocen
+
+    def is_passed(self):
+        average = sum(self.marks) / len(self.marks)
+        return average > 50
+# Przykładowe obiekty klasy Student
+
+student1 = Student("Jan", [60, 70, 80])     # średnia = 70 → True
+student2 = Student("Anna", [30, 40, 50])    # średnia = 40 → False
+
+# Sprawdzenie wyników
+print(student1.name, "passed:", student1.is_passed())
+print(student2.name, "passed:", student2.is_passed())
+
+
