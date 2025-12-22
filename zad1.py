d2f1106790294c82dd2ad49a624a71da2d21f713
piotr schuetz <piotr.schuetz@edu.uekat.pl>
2025-12-22 18:48:13 +0100

    aktualizacja danych

--- zad1.py
+++ zad1.py
@@ -3,6 +3,7 @@ def print_names(names):
     for name in names:
         print(name)
 
+
 print_names( ["tomasz", "arkadiusz", "mateusz", "damian", "stanisław"])
 
 #lista liczb przemnożonych przez 2 pętla for
@@ -15,31 +16,32 @@ def multiply_numbers_by_two(numbers):
 
     return numbers_multiplied_by_two
 
+
 print(multiply_numbers_by_two([1,2,3,4,5]))
 
 #lista liczb przemnożonych przez 2 lista składana
 def multiply_numbers_by_two_using_list_comprehension(numbers):
     return [number * 2 for number in numbers]
 
-print (multiply_numbers_by_two_using_list_comprehension([1,2,3,4,5]))
 
+print(multiply_numbers_by_two_using_list_comprehension([1,2,3,4,5]))
 
 # lista 10 cyfr wyświetla liczby parzyste
-
-def even_numbers(numbers1):
-    for number1 in numbers1:
-        if number1 % 2 ==0:
-            print(number1)
-even_numbers(list(range(1,11)))
+def print_even_numbers(numbers):
+    for number in numbers:
+        if number % 2 == 0:
+            print(number)
 
 
+print_even_numbers(list(range(1, 11)))
 
 #lista 10 cyfr wyświetla co drugi element
+def print_every_other_element(numbers):
+    for i in range(1, len(numbers), 2):
+        print(numbers[i])
 
-def every_other_element(numbers2):
-    for i in range(1, len(numbers2), 2):
-        print(numbers2[i])
-every_other_element(list(range(13,23)))
+
+print_every_other_element(list(range(13, 23)))
 
 
 
