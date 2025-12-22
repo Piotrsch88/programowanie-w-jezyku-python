dc36ccc7a0f5046671d9eeaa17d05c23f86205cc
piotr schuetz <piotr.schuetz@edu.uekat.pl>
2025-12-22 19:30:37 +0100

    Aktualizacja danych

--- Zad1operatory.py
+++ Zad1operatory.py
@@ -1,6 +1,9 @@
 # Stworzenie funkcji która przyjmuje 2 agrumenty typu string
-name = "{Jan}"
-surname = "{Kowalski}"
+name = "Jan"
+surname = "Kowalski"
 def type_name(name: str, surname: str) -> str:
-     return name + " " + surname
-print(f"Cześć {type_name(name, surname)}!")
+    return f"Cześć {name} {surname}!"
+
+
+greetings = type_name(name, surname)
+print(greetings)
